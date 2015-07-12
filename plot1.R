6d3dee73b513b3ac4ee17f355a4952c1675e1d36
jrobertscott <jrobertscott@gmail.com>
2015-07-12 16:03:17 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,7 +3,8 @@ data1 <-data[, c(1,3)]
 data1$Date2 <-as.Date(data1$Date, "%d/%m/%Y")
 data2 <-subset(data1, data1$Date2 == "2007-02-01" | data1$Date2 == "2007-02-02")
 quartz()
+png(file="plot1.png")
 hist(as.numeric(paste(data2$Global_active_power)), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-
+dev.off()
 
 
